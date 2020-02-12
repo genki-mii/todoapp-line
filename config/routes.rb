d10ca1cd90c792e2b7fdaa9f25e7fbf1c90e502b
EC2 Default User <ec2-user@ip-172-31-30-125.us-east-2.compute.internal>
2020-02-12 12:52:36 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/:id/edit'=>'tasks#edit',
+    as:"task_edit"
+    post'tasks/:id/update'=>'task#update',
+      as:"task_update"
+  delete'task/:id/delete'=>'tasks#delete',
+    as:"task_delete"
+  post 'line/bot'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
